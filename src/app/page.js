6a1d1033d4f15f80ef25864a098aaf93288d1d73
P94Vansh <vanshgambhirag@gmail.com>
2025-09-05 12:13:11 +0530

    Added popup in the website

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 'use client'
 import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
-import React from "react";
+import React,{useState} from "react";
 import { BackgroundLines } from "@/components/ui/background-lines";
 import { Button } from "@/components/ui/moving-border";
 import ImageBox from "@/components/CardGuests";
@@ -8,9 +8,11 @@ import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect"
 import { SparklesCore } from "@/components/ui/sparkles";
 import { CardStack } from "@/components/ui/card-stack";
 import Image from "next/image";
+import { IoClose } from "react-icons/io5";
 import { useRouter } from "next/navigation";
 export default function Home() {
-  const router=useRouter()
+   const [open,setOpen]=useState(true);
+  const router = useRouter()
   const CARDS1 = [
     {
       id: 0,
@@ -69,53 +71,73 @@ export default function Home() {
   ]
   const words = 'Where Innovation meets Elevation'
   return (
-    <>  
+    <>
       <div
-      className="relative h-screen w-full 
+        className="relative h-screen w-full 
                  bg-[url('/mobile.jpg')] md:bg-[url('/desktop.jpg')] 
                  bg-cover bg-center"
-    >
-      {/* Overlay for readability */}
-      <div className="absolute inset-0 bg-black/40"></div>
+      >
+        {/* Overlay for readability */}
+        <div className="absolute inset-0 bg-black/40"></div>
+        {open && (
+        <div className="fixed inset-0 flex items-center justify-center z-50  bg-opacity-50">
+          <div
+            className="relative bg-white p-5 rounded-lg shadow-lg w-[80%] max-w-md 
+                       transform transition-all duration-500 opacity-0 scale-95 animate-fadeIn"
+          >
+            <IoClose
+              className="hover:cursor-pointer absolute top-2 right-2 text-3xl text-red-700 font-bold"
+              onClick={() => setOpen(false)}
+            />
+            <div className="flex items-center justify-center">
+              <img
+                src="popup.gif"
+                alt="Popup"
+                className="object-contain h-full w-full p-3"
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    
+        {/* Content */}
+        <div className="relative flex flex-col items-center justify-center h-full px-4 text-center">
+          <h2 className="bg-clip-text text-transparent bg-gradient-to-b  from-neutral-200 to-white text-3xl md:text-5xl lg:text-5xl font-sans py-2 md:py-10 font-bold tracking-tight">
+            Engineers&apos; Day Mahotsava <br /> 2.0
+          </h2>
 
-      {/* Content */}
-      <div className="relative flex flex-col items-center justify-center h-full px-4 text-center">
-        <h2 className="bg-clip-text text-transparent bg-gradient-to-b  from-neutral-200 to-white text-3xl md:text-5xl lg:text-5xl font-sans py-2 md:py-10 font-bold tracking-tight">
-          Engineers&apos; Day Mahotsava <br /> 2.0
-        </h2>
+          <TextGenerateEffect words={words} />
 
-        <TextGenerateEffect words={words} />
-
-        <h2 className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-900 to-neutral-700 dark:text-white md:text-2xl lg:text-3xl font-sans py-2 md:pt-10 font-bold tracking-tight">
-          Organised By:
-        </h2>
-        <p
-  className="max-w-xl mx-auto text-md md:text-2xl text-white text-center
+          <h2 className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-900 to-neutral-700 dark:text-white md:text-2xl lg:text-3xl font-sans py-2 md:pt-10 font-bold tracking-tight">
+            Organised By:
+          </h2>
+          <p
+            className="max-w-xl mx-auto text-md md:text-2xl text-white text-center
              px-6 py-2 rounded-full 
              bg-white/20 backdrop-blur-sm"
->
-  CORE COMMITTEE UIT
-</p>
+          >
+            CORE COMMITTEE UIT
+          </p>
 
 
-        <div className="flex md:flex-row flex-col gap-5 pt-4 mt-3">
-          <Button
-            borderRadius="1.75rem"
-            onClick={() => router.push("/technical-events")}
-            className="bg-white cursor-pointer dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
-          >
-            Technical Events
-          </Button>
-          <Button
-            borderRadius="1.75rem"
-            onClick={() => router.push("/non-technical-events")}
-            className="bg-white cursor-pointer dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
-          >
-            Non-Technical Events
-          </Button>
+          <div className="flex md:flex-row flex-col gap-5 pt-4 mt-3">
+            <Button
+              borderRadius="1.75rem"
+              onClick={() => router.push("/technical-events")}
+              className="bg-white cursor-pointer dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+            >
+              Technical Events
+            </Button>
+            <Button
+              borderRadius="1.75rem"
+              onClick={() => router.push("/non-technical-events")}
+              className="bg-white cursor-pointer dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+            >
+              Non-Technical Events
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
       <hr className="bg-white h-0.5" />
       <div className="w-full rounded-md flex flex-col md:justify-center  antialiased bg-grid-white/[0.02] relative overflow-hidden items-center py-10 ">
         <TypewriterEffectSmooth words={[{ text: 'Why' }, { text: "Engineers'" }, { text: "Day?" }]} />
@@ -170,24 +192,24 @@ export default function Home() {
         <Image src={'/UuLogo.jpg'} className="bg-white p-5 rounded-2xl" alt="University Logo" width={300} height={300} />
         <TypewriterEffectSmooth words={[{ text: 'About' }, { text: 'Uttaranchal' }, { text: 'Institute' }, { text: 'Of' }, { text: 'Technology' }]} />
         <div className="text-white text-justify font-serif md:text-2xl text-md p-3">Uttaranchal Institute of Technology (UIT), established in 2006 in Dehradun, is a premier engineering college under Uttaranchal University. UIT offers industry-aligned B.Tech and M.Tech programs, fostering innovation, research, and leadership. With modern infrastructure, expert faculty, and strong placement records, UIT empowers students to become globally competitive professionals. Its vibrant campus culture and academic excellence make it a top choice for aspiring engineers in Uttarakhand.</div>
-      <div className="w-[40rem] h-40 relative">
-        {/* Gradients */}
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
-        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
-        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
-        <SparklesCore
-          background="transparent"
-          minSize={0.4}
-          maxSize={1}
-          particleDensity={1200}
-          className="w-full h-full"
-          particleColor="#FFFFFF"
+        <div className="w-[40rem] h-40 relative">
+          {/* Gradients */}
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+          <SparklesCore
+            background="transparent"
+            minSize={0.4}
+            maxSize={1}
+            particleDensity={1200}
+            className="w-full h-full"
+            particleColor="#FFFFFF"
           />
 
-        {/* Radial Gradient to prevent sharp edges */}
-        <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
-          </div>
+          {/* Radial Gradient to prevent sharp edges */}
+          <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+        </div>
       </div>
       <hr className="bg-white h-0.5" />
       <div className="md:h-[50rem] h-[60rem] container mx-auto w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
@@ -232,7 +254,7 @@ export default function Home() {
         <CardStack items={CARDS2} />
         <CardStack items={CARDS3} />
       </div>
-      <div className="h-20 bg-black"/>
+      <div className="h-20 bg-black" />
       <hr className="bg-white h-0.5" />
 
     </>
